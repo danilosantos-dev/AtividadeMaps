3016c55c7d9f6a1c710929203ccaf45e86592c65
Danilo Santos <santosdanilo2024@gmail.com>
2023-05-23 21:45:20 -0300

    Criado maps e adicionado geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
+import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
+  map!: GoogleMap;
 
-  constructor() {}
+  constructor() { }
+
+  ionViewWillEnter() {
+      this.createMap();
+  }
+
+  async createMap() {
+      this.map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+          center: {
+              lat: -22.489271,
+              lng: -48.546447,
+          },
+          zoom: 1,
+      },
+  });
+  this.buscarPosicao();
+}
+
+  async buscarPosicao() {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
+      this.adicionarMarcador(coordinates);
+      return coordinates;
+  }
+
+   async adicionarMarcador(coordinates: Position) {
+      // Add a marker to the map
+      const markerId = await this.map.addMarker({
+          coordinate: {
+              lat: coordinates.coords.latitude,
+              lng: coordinates.coords.longitude
+          }      
+      });
+      this.zoomNoMarcador(coordinates);
+  }
+
+zoomNoMarcador(coordinates: Position){
+  this.map.setCamera({
+    coordinate:{
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    },
+  zoom:15,
+  animate: true
+  })
+}
+
 
 }
